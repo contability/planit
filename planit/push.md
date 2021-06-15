acd0fbf9e0011365ae459ddd06a0b76c5c7761ab
furium <s_by_d@naver.com>
2021-06-16 00:18:30 +0900

    20210616 furium

--- planit/push.md
+++
@@ -1,8 +0,0 @@
-<h1>PLANIT</h1><br>
-	<h3>Java 1.8.0_192</h3><br>
-	<h3>eGovFrameDev-3.10.0</h3><br>
-	<h3>Oracle DB</h3><br>
-	<h3>apache-tomcat-9.0.43</h3><br>
-	<h3>Spring Tool Suite 3.9.15</h3><br>
-	<h3>Maven 3.5.1</h3><br>
-	
--- planit/src/main/java/kr/co/planit/controller/LoginController.java
+++ planit/src/main/java/kr/co/planit/controller/LoginController.java
@@ -1,18 +1,29 @@
 package kr.co.planit.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import kr.co.planit.service.PlnUserService;
+
 @Controller
 public class LoginController {
 	
-//	@Resource
-//	private PlnUserService plnUserService;
+	@Resource
+	private PlnUserService plnUserService;
 	
 	@RequestMapping(value = "/portal.go", method = RequestMethod.GET)
 	public String loginPage() {
-		System.out.println("anwlagnwanglni");
+		System.out.println("\n\n\n\n\n\n anwlagnwanglni");
 		return "portal";
+	}
+	
+	@RequestMapping("/test.go")
+	public String test() {
+		System.out.println("\n\n\n\n\n\n success....");
+		
+		return "";
 	}
 }
